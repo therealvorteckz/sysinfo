1f0c9aab50f3181a9d96f42a1e5028d4809dca6d
vorteckz <48614417+therealvorteckz@users.noreply.github.com>
2024-02-17 23:39:12 -0500

    Update sysinfo.py

--- sysinfo.py
+++ sysinfo.py
@@ -14,8 +14,8 @@ import socket
 
 # Formatting Control Characters / Color Codes / ( do not alter reset )
 reset       = '\x0f'
-color1       = '04' # red
-color2       = '14' # grey
+color1       = '14' # bracket close/open = grey
+color2       = '04' # color for labels = red
 
 # IRC Color
 def color(msg: str, foreground: str, background: str='') -> str:
